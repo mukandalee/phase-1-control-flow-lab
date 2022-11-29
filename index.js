2467aa24df3b26e7cd15aafba128e6d41ecf5d66
mukandalee <leonardmukanda254@gmail.com>
2022-11-29 19:21:37 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(rideFeet){
+  let result
+ if( rideFeet<= 400)
+ {
+   return result='This one is on me!';
+   
+ }
+ else if(rideFeet>2000 && rideFeet<2500){
+  
+   return result='I will gladly take your thirty bucks.';
+ }
+ else if(rideFeet>2500){
+   
+   return result="No can do."
+ }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+ let outcome;
+ if (city=="NYC"){
+  return "Ok, sounds good."
+ }
+ else{
+  return 'No go.'
+ }
+
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+//  let tip = 'generous';
+
+let feedback;
+
+switch (tip) {
+   case 'generous':
+       return feedback = "Thank you so much.";
+       break;
+   case 'not as generous':
+       return feedback = "Thank you.";
+       break;
+   default:
+       return  feedback="Bye."
+}
+
+
 }
